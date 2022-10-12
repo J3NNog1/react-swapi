f90c3380121b5d3d37efca1e0c24196d233a3671
Jenn <jenniferjuarezjj@gmail.com>
2022-10-12 16:43:23 -0400

    implement nav bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (  
+    <div className='App-header'>
+      <p>
+        STAR WARS STARSHIPS
+      </p>
+    </div>
+  );
+}
+ 
+export default Header;
---
+++ src/services/sw-api.js
@@ -0,0 +1,7 @@
+const baseUrl = 'https://swapi.dev/api/starships'
+
+export async function getAllStarships() {
+  const res = await fetch(baseUrl)
+  return res.json()
+}
+
